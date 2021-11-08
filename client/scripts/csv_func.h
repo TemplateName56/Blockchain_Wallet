5dd78703f87ec831a1cd6f0d890abfc0d784627f
Vexel-dev <eugenelatyshew@gmail.com>
2021-11-08 10:34:40 +0300

    CSV update
    Класс csv

--- client/scripts/csv_func.h
+++ client/scripts/csv_func.h
@@ -1,8 +1,28 @@
 #ifndef CSV_FUNC_H
 #define CSV_FUNC_H
+#include <QCoreApplication>
+#include <iostream>
+#include <QFile>
+#include <QtDebug>
 
+#include <QDate>
+#include <QTime>
+#include <QDateTime>
 
-void readCSV();
-void writeCSV();
+using namespace std;
+
+class CSV {
+public:
+    CSV(QString fileName);
+    void append_csv_request(QString date, QString message, QString label, double amount);
+    QString get_message_csv(int row);
+    QString get_label_csv(int row);
+    QString get_date_csv(int row);
+    QString get_money_csv(int row);
+
+private:
+     QString filename;
+};
+
 
 #endif // CSV_FUNC_H
