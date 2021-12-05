a66c8db68b2d72c73b48f7cab290873abd26cc26
daniil <adidanka111@gmail.com>
2021-12-05 18:21:03 +0300

    Хеш функия
    Исправление хеш функции

--- client/scripts/program_algorithms.cpp
+++ client/scripts/program_algorithms.cpp
@@ -19,35 +19,32 @@ void algoritms::Sort(int *arr,int n)
 string algoritms::Hash(string str)
 {
     string rezult;
-    int minimallenght = 2;//длина 2^n степени
-    int realminimallenght = 0;
-    int lenghtstr = (str.size()); // длинна строки
-    while (minimallenght <= 16)//получение размера строки 32 символа
-    {
-        realminimallenght = (minimallenght *= 2);
-    }
-    if ((minimallenght - lenghtstr) < minimallenght) //получение минимально длинны превышающей строку
-    {
-        minimallenght *= 2;
-    }
-    int Count = minimallenght - lenghtstr;
+        int minimallenght = 32;//длина 2^n степени
+        int lenghthash = 16;
+        int lenghtstr = (str.size()); // длинна строки
 
-    for (int i = 0; i < Count; i++)
-    {
-        str += ConvertIntoLetter(str[i] + str[i + 1]);
-    }
-
-    int lenghthash=realminimallenght-16;
-    while (str.size() != lenghthash) // получение хеша длинной 16 символов
-    {
-        for (int i = 0, center = str.size() / 2; i < center; i++)
+        if ((minimallenght - lenghtstr) < minimallenght) //получение минимально длинны превышающей строку
         {
-            rezult += ConvertIntoLetter(str[center - i] + str[center + i]);
+            minimallenght *= 2;
         }
-        str = rezult;
-        rezult.clear();
-    }
-    return str;
+        int Count = minimallenght - lenghtstr;
+
+        for (int i = 0; i < Count; i++)
+        {
+            str += ConvertIntoLetter(str[i] + str[i + 1]);
+        }
+
+
+        while (str.size() != lenghthash) // получение хеша длинной 16 символов
+        {
+            for (int i = 0, center = str.size() / 2; i < center; i++)
+            {
+                rezult += ConvertIntoLetter(str[center - i] + str[center + i]);
+            }
+            str = rezult;
+            rezult.clear();
+        }
+        return str;
 }
 
 int algoritms::ConvertIntoLetter(int rezult)
