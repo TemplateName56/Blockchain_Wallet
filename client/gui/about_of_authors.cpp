f937553b9dde8c8256f84e2ca6d70125f44beacb
Nikita Khalizev <deabrua.sendoa@gmail.com>
2021-12-15 15:47:54 +0200

    Test

--- client/gui/about_of_authors.cpp
+++ client/gui/about_of_authors.cpp
@@ -1,6 +1,8 @@
 #include "about_of_authors.h"
 #include "ui_about_of_authors.h"
 
+#include "settings_form.h"
+
 about_of_authors::about_of_authors(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::about_of_authors)
@@ -31,22 +33,23 @@ void about_of_authors::setWindowLanguage(int index)
 {
     switch (index) {
 
-       //case ENGLISH:
+       case ENGLISH:
 
-    //ui->textEdit_about_of_authors->setText("<html><head/><body><p><span style=\" font-style:italic;\">BlockChain Wallet</span> was created by a group of four students who are currently studying at <span style=\" font-style:italic;\">Zaporizhzhya Polytechnic National University</span>. </p><p><br/></p><p>The ideological inspirer was <span style=\" font-weight:600;\">Nikita Khalizev</span>, the work on the software functionality was done by <span style=\" font-weight:600;\">Daniil Potapov</span>, the interaction between the files and the program was organized by <span style=\" font-weight:600;\">Evgeny Latyshev</span>, and the visual component was responsible for <span style=\" font-weight:600;\">Azmanov Ilya</span>.</p><p><br/></p><p><span style=\" font-style:italic;\">The goal of BlockChain Wallet</span> is to popularize the use of electronic wallets among <span style=\" font-style:italic;\">NUZP</span> students.</p><p><br/></p><p><br/></p></body></html>");
+    ui->textEdit_about_of_authors->setText("<html><head/><body><p><span style=\" font-style:italic;\">BlockChain Wallet</span> was created by a group of four students who are currently studying at <span style=\" font-style:italic;\">Zaporizhzhya Polytechnic National University</span>. </p><p><br/></p><p>The ideological inspirer was <span style=\" font-weight:600;\">Nikita Khalizev</span>, the work on the software functionality was done by <span style=\" font-weight:600;\">Daniil Potapov</span>, the interaction between the files and the program was organized by <span style=\" font-weight:600;\">Evgeny Latyshev</span>, and the visual component was responsible for <span style=\" font-weight:600;\">Azmanov Ilya</span>.</p><p><br/></p><p><span style=\" font-style:italic;\">The goal of BlockChain Wallet</span> is to popularize the use of electronic wallets among <span style=\" font-style:italic;\">NUZP</span> students.</p><p><br/></p><p><br/></p></body></html>");
 
-           // break;
+            break;
 
-    //    case UKRANIAN:
+        case UKRANIAN:
 
-   //ui->textEdit_about_of_authors->setText("<html><head/><body><p><span style=\" font-style:italic;\">BlockChain Wallet</span> був створений групою з чотирьох студентів, які на даний момент навчаються в <span style=\" text-decoration: underline;\">Національному університеті &quot;Запорізька політехніка&quot;</span>. </p><p><br/></p><p>Ідейним натхненником виступав <span style=\" font-weight:600;\">Микита Хализев</span>, роботою над функціоналом ПЗ займався <span style=\" font-weight:600;\">Данило Потапов</span>, взаємодії між файлами і програмою організовував <span style=\" font-weight:600;\">Євген Латишев</span>, за візуальну складову відповідав <span style=\" font-weight:600;\">Азманов Ілля</span>.</p><p><br/></p><p><span style=\" font-style:italic;\">Мета BlockChain Wallet</span> полягає в тому, популяризувати використання електронних гаманців серед студентів <span style=\" text-decoration: underline;\">НУЗП</span>.</p></body></html>");
+        this->setWindowTitle("dsadasdsa");
+   ui->textEdit_about_of_authors->setText("<html><head/><body><p><span style=\" font-style:italic;\">BlockChain Wallet</span> був створений групою з чотирьох студентів, які на даний момент навчаються в <span style=\" text-decoration: underline;\">Національному університеті &quot;Запорізька політехніка&quot;</span>. </p><p><br/></p><p>Ідейним натхненником виступав <span style=\" font-weight:600;\">Микита Хализев</span>, роботою над функціоналом ПЗ займався <span style=\" font-weight:600;\">Данило Потапов</span>, взаємодії між файлами і програмою організовував <span style=\" font-weight:600;\">Євген Латишев</span>, за візуальну складову відповідав <span style=\" font-weight:600;\">Азманов Ілля</span>.</p><p><br/></p><p><span style=\" font-style:italic;\">Мета BlockChain Wallet</span> полягає в тому, популяризувати використання електронних гаманців серед студентів <span style=\" text-decoration: underline;\">НУЗП</span>.</p></body></html>");
 
-             //        break;
+                     break;
 
-    //    case RUSSIAN:
-    //
-    // ui->textEdit_about_of_authors->setText("<html><head/><body><p><span style=\" font-style:italic;\">BlockChain Wallet</span> был создан группой из четверых студентов, которые на данный момент учатся в <span style=\" text-decoration: underline;\">Национальном университете &quot;Запорожская Политехника&quot;</span>. </p><p><br/></p><p>Идейным вдохновителем выступал <span style=\" font-weight:600;\">Никита Хализев</span>, работой над функционалом ПО занимался <span style=\" font-weight:600;\">Даниил Потапов</span>, взаимодействия между файлами и программой организовывал <span style=\" font-weight:600;\">Евгений Латышев</span>, за визуальную составляющую отвечал<span style=\" font-weight:600;\"> Азманов Илья</span>.</p><p><br/></p><p><span style=\" font-style:italic;\">Цель BlockChain Wallet</span> состоит в том, популяризировать использование электронных кошельков среди студентов <span style=\" text-decoration: underline;\">НУЗП</span>.</p></body></html>");
+        case RUSSIAN:
 
-    //        break;
+     ui->textEdit_about_of_authors->setText("<html><head/><body><p><span style=\" font-style:italic;\">BlockChain Wallet</span> был создан группой из четверых студентов, которые на данный момент учатся в <span style=\" text-decoration: underline;\">Национальном университете &quot;Запорожская Политехника&quot;</span>. </p><p><br/></p><p>Идейным вдохновителем выступал <span style=\" font-weight:600;\">Никита Хализев</span>, работой над функционалом ПО занимался <span style=\" font-weight:600;\">Даниил Потапов</span>, взаимодействия между файлами и программой организовывал <span style=\" font-weight:600;\">Евгений Латышев</span>, за визуальную составляющую отвечал<span style=\" font-weight:600;\"> Азманов Илья</span>.</p><p><br/></p><p><span style=\" font-style:italic;\">Цель BlockChain Wallet</span> состоит в том, популяризировать использование электронных кошельков среди студентов <span style=\" text-decoration: underline;\">НУЗП</span>.</p></body></html>");
+
+            break;
         }
 }
